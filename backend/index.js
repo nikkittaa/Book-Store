181b6d748da06074e5f1d422f90fed6f7b5166c4
Nikita <nikitanb04@gmail.com>
2023-12-03 17:32:13 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import {PORT, mongoDBURL} from './config.js';
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+/*
+app.use(cors({
+    origin: 'https://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+}));
+*/
+
+app.get('/', (request, response) =>{
+     return response.status(234).send("Book store");
+});
+
+app.use('/books', booksRoute);
+
+mongoose.connect(mongoDBURL)
+    .then(() =>{
+        console.log("App connected to database");
+        app.listen(PORT , () =>{
+            console.log(`App is listening on ${PORT}`);
+        });
+    })
+    .catch((error) =>{
+        console.log(error);
+    });
+
+
